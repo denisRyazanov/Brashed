83e5943179c85f44d78d789017195b9945939000
Denis <nppteploautomat2000@gmail.com>
2017-06-21 16:04:20 +0300

    21/06_01

--- app/js/user.js
+++ app/js/user.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	$('.slider').slick();
-});
+	$('.one-time').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+}); 
